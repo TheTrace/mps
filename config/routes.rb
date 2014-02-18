0761919c20d55ad821802229813f13b2b9d7d7b0
Alan Tracey <actracey@gmail.com>
2014-02-18 15:56:28 +0000

    update routes for heroku

--- config/routes.rb
+++ config/routes.rb
@@ -63,5 +63,5 @@ Mps::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-  match '/', to: => 'jobs#index', via: 'get'
+  match '/', to: 'jobs#index', via: 'get'
 end
