34ffea35655259321ca436113d461f6ab2e7cca5
Alan Tracey <actracey@gmail.com>
2014-04-15 22:58:51 +0100

    Start to finances and loggin in

--- app/controllers/finances_controller.rb
+++ app/controllers/finances_controller.rb
@@ -1,5 +1,21 @@
 class FinancesController < ApplicationController
+	before_action :set_job, only: [:show, :edit, :update, :destroy]
+
 	def index
 		@jobs = Job.all
 	end
+
+	def show
+	end
+
+	private
+		# Use callbacks to share common setup or constraints between actions.
+		def set_job
+			@job = Job.find(params[:id])
+		end
+
+		# Never trust parameters from the scary internet, only allow the white list through.
+		def finance_params
+			params.require(:job).permit(:title, :text, :reference, :party_a_id, :party_b_id, :legal_rep, :mediator, :fees_paid, :mediation_date)
+		end
 end
